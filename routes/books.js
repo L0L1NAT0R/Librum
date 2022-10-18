00f66fd3f7a0b9719cdfc4af0ceb14f7c540bce7
SasitWongsakdi <mattheww_2023@concordian.org>
2022-10-18 23:36:57 +0700

    lol

--- routes/books.js
+++ routes/books.js
@@ -43,41 +43,30 @@ router.post('/', async (req, res) => {
   })
   saveCover(book, req.body.cover)
 
-  try {
+
     const newBook = await book.save()
-    res.redirect(`books/${newBook.id}`)
-  } catch {
-    renderNewPage(res, book, true)
-  }
+    res.redirect(`books/${newBook.id}`) 
 })
 
 // Show Book Route
 router.get('/:id', async (req, res) => {
-  try {
     const book = await Book.findById(req.params.id)
                            .populate('author')
                            .exec()
     res.render('books/show', { book: book })
-  } catch {
-    res.redirect('/')
-  }
+
 })
 
 // Edit Book Route
 router.get('/:id/edit', async (req, res) => {
-  try {
     const book = await Book.findById(req.params.id)
     renderEditPage(res, book)
-  } catch {
-    res.redirect('/')
-  }
 })
 
 // Update Book Route
 router.put('/:id', async (req, res) => {
   let book
 
-  try {
     book = await Book.findById(req.params.id)
     book.title = req.body.title
     book.author = req.body.author
@@ -89,32 +78,16 @@ router.put('/:id', async (req, res) => {
     }
     await book.save()
     res.redirect(`/books/${book.id}`)
-  } catch {
-    if (book != null) {
-      renderEditPage(res, book, true)
-    } else {
-      redirect('/')
-    }
-  }
+
+
 })
 
 // Delete Book Page
 router.delete('/:id', async (req, res) => {
   let book
-  try {
     book = await Book.findById(req.params.id)
     await book.remove()
     res.redirect('/books')
-  } catch {
-    if (book != null) {
-      res.render('books/show', {
-        book: book,
-        errorMessage: 'Could not remove book'
-      })
-    } else {
-      res.redirect('/')
-    }
-  }
 })
 
 async function renderNewPage(res, book, hasError = false) {
@@ -148,7 +121,7 @@ async function renderFormPage(res, book, form, hasError = false) {
 function saveCover(book, coverEncoded) {
   if (coverEncoded == null) return
   const cover = JSON.parse(coverEncoded)
-  if (cover != null && imageMimeTypes.includes(cover.type)) {
+  if (imageMimeTypes.includes(cover.type)) {
     book.coverImage = new Buffer.from(cover.data, 'base64')
     book.coverImageType = cover.type
   }
